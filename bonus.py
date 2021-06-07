3bc56eaf44e467a0778d2db6db529a2c4d7086e1
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-06-07 12:15:46 +0300

    changed bonus matplotlib settings

--- bonus.py
+++ bonus.py
@@ -1,4 +1,5 @@
-import matplotlib.pyplot as plt
+import matplotlib as plt
+plt.use('Agg')
 from sklearn import datasets
 from sklearn.cluster import KMeans
 
@@ -10,19 +11,19 @@ def main():
         kmeans = KMeans(n_clusters=k, init='k-means++', random_state=0).fit(iris)
         inertias.append(kmeans.inertia_)
     
-    plt.plot(range(1, 11), inertias)
-    plt.title('Elbow Method for selection of optimal "K" clusters')
-    plt.xlabel('K')
-    plt.ylabel('Average Dispersion')
+    plt.pyplot.plot(range(1, 11), inertias)
+    plt.pyplot.title('Elbow Method for selection of optimal "K" clusters')
+    plt.pyplot.xlabel('K')
+    plt.pyplot.ylabel('Average Dispersion')
     
     circle_rad = 15  # This is the radius, in points
-    plt.plot(3, inertias[2], 'o',
+    plt.pyplot.plot(3, inertias[2], 'o',
         ms=circle_rad * 2, mec='b', mfc='none', mew=2)
-    plt.annotate("Elbow Point",xy=(3,inertias[2]),xytext=(4,150),
+    plt.pyplot.annotate("Elbow Point",xy=(3,inertias[2]),xytext=(4,150),
                  arrowprops=dict(arrowstyle='simple',shrinkB=circle_rad*1.2))
     
     
-    plt.savefig('elbow.png')
+    plt.pyplot.savefig('elbow.png')
     #plt.show()
 
 

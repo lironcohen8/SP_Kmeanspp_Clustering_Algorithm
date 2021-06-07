a4ee9aaa696efddeff8562268181f6014d69de33
Shaked Lubin <shaked.lubin@gmail.com>
2021-06-07 13:38:43 +0300

    Bonus working on nova

--- bonus.py
+++ bonus.py
@@ -1,5 +1,6 @@
-import matplotlib as plt
-plt.use('Agg')
+import matplotlib 
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.cluster import KMeans
 
@@ -11,19 +12,19 @@ def main():
         kmeans = KMeans(n_clusters=k, init='k-means++', random_state=0).fit(iris)
         inertias.append(kmeans.inertia_)
     
-    plt.pyplot.plot(range(1, 11), inertias)
-    plt.pyplot.title('Elbow Method for selection of optimal "K" clusters')
-    plt.pyplot.xlabel('K')
-    plt.pyplot.ylabel('Average Dispersion')
+    plt.plot(range(1, 11), inertias)
+    plt.title('Elbow Method for selection of optimal "K" clusters')
+    plt.xlabel('K')
+    plt.ylabel('Average Dispersion')
     
     circle_rad = 15  # This is the radius, in points
-    plt.pyplot.plot(3, inertias[2], 'o',
+    plt.plot(3, inertias[2], 'o',
         ms=circle_rad * 2, mec='b', mfc='none', mew=2)
-    plt.pyplot.annotate("Elbow Point",xy=(3,inertias[2]),xytext=(4,150),
+    plt.annotate("Elbow Point",xy=(3,inertias[2]),xytext=(4,150),
                  arrowprops=dict(arrowstyle='simple',shrinkB=circle_rad*1.2))
     
     
-    plt.pyplot.savefig('elbow.png')
+    plt.savefig('elbow.png')
     #plt.show()
 
 
